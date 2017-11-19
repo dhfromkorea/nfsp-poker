ac39f6afc1e6b07d9dcfde12af2533e967cd94eb
Sebastien Baur <sebastien.baur@hotmail.fr>
2017-11-18 22:11:48 -0500

    card featurizer

--- Programs/models/q_network.py
+++ Programs/models/q_network.py
@@ -22,21 +22,18 @@ def flatten(x):
     return x.resize(shape[0], int(np.prod(shape[1:])))
 
 
-class SharedNetwork(t.nn.Module):
-    def __init__(self, n_actions, hidden_dim):
-        super(SharedNetwork, self).__init__()
-        self.n_actions = n_actions
-        self.hidden_dim = hidden_dim
-        hdim = hidden_dim
-
+class CardFeaturizer(t.nn.Module):
+    def __init__(self, hdim):
+        super(CardFeaturizer, self).__init__()
+        self.hdim = hdim
         self.conv1 = conv(2, 5, 1)
         self.conv2 = conv(5, 1, 5, padding=2)
         self.conv3 = conv(5, 1, 3, padding=1)
         self.conv4 = conv(5, 3, 3, dilation=2, padding=2)
         self.conv5 = conv(2, 1, 1)
 
-        self.fc1 = fc(13*5, hdim)
-        self.fc2 = fc(13*2, hdim)
+        self.fc1 = fc(13 * 5, hdim)
+        self.fc2 = fc(13 * 2, hdim)
         self.fc3 = fc(hdim, hdim)
         self.fc4 = fc(4, hdim)
         self.fc5 = fc(52, hdim)
@@ -53,20 +50,8 @@ class SharedNetwork(t.nn.Module):
         self.fc16 = fc(hdim, 9)  # 9 is the number of combinations (high card, pair, two pairs, trips, straight, flush, full house, quads, straight flush
         self.fc17 = fc(hdim, 9)
         self.fc18 = fc(hdim, 1)
-        self.fc19 = fc(5 * 6 * 2, hdim)
-        self.fc20 = fc(5 * 6 * 2 + hdim, hdim)
-        self.fc21 = fc(5 * 6 * 2 + hdim, hdim)
-        self.fc22 = fc(5 * 6 * 2 + hdim, hdim)
-        self.fc23 = fc(hdim, hdim)
-        self.fc24 = fc(5, hdim)
-        self.fc25 = fc(3 * hdim, hdim)
-        self.fc26 = fc(hdim, hdim)
 
-    def forward(self, hand, board, pot, stack, opponent_stack, big_blind, dealer, preflop_plays, flop_plays, turn_plays, river_plays):
-        hdim = self.hidden_dim
-        n_actions = self.n_actions
-
-
+    def forward(self, hand, board):
         # DETECTING PATTERNS IN THE BOARD AND HAND
         # Aggregate by suit and kind
         color_hand = t.sum(hand, 1)
@@ -98,9 +83,9 @@ class SharedNetwork(t.nn.Module):
         flop_alone = selu(self.fc5(flatten(board[:, 0, :, :])))
         flop_alone = selu(self.fc6(flop_alone))
         turn_alone = selu(self.fc7(flatten(board[:, 1, :, :])))
-        turn_alone = selu(self.fc8(flop_alone))
+        turn_alone = selu(self.fc8(turn_alone))
         river_alone = selu(self.fc9(flatten(board[:, 2, :, :])))
-        river_alone = selu(self.fc10(flop_alone))
+        river_alone = selu(self.fc10(river_alone))
         board_alone = selu(self.fc11(t.cat([flop_alone, turn_alone, river_alone], -1)))
 
         # Process board and hand together with FC layers
@@ -113,22 +98,41 @@ class SharedNetwork(t.nn.Module):
         probabilities_of_each_combination_board_only = softmax(self.fc16(board_alone))
         probabilities_of_each_combination_board_hand = softmax(self.fc17(bh))
         hand_strength = sigmoid(self.fc18(bh))
+        return hand_strength, probabilities_of_each_combination_board_hand, probabilities_of_each_combination_board_only, flop_alone, turn_alone, river_alone, bh
 
+
+class SharedNetwork(t.nn.Module):
+    def __init__(self, n_actions, hidden_dim):
+        super(SharedNetwork, self).__init__()
+        self.n_actions = n_actions
+        self.hidden_dim = hidden_dim
+        hdim = hidden_dim
+
+        self.fc19 = fc(5 * 6 * 2, hdim)
+        self.fc20 = fc(5 * 6 * 2 + hdim, hdim)
+        self.fc21 = fc(5 * 6 * 2 + hdim, hdim)
+        self.fc22 = fc(5 * 6 * 2 + hdim, hdim)
+        self.fc23 = fc(hdim, hdim)
+        self.fc24 = fc(5, hdim)
+        self.fc25 = fc(3 * hdim, hdim)
+        self.fc26 = fc(hdim, hdim)
+
+    def forward(self, cards_features, flop_features, turn_features, river_features, board_hand_features, pot, stack, opponent_stack, big_blind, dealer, preflop_plays, flop_plays, turn_plays, river_plays):
         # PROCESS THE ACTIONS THAT WERE TAKEN IN THE CURRENT EPISODE
         processed_preflop = selu(self.fc19(flatten(preflop_plays)))
-        processed_flop = selu(self.fc20(t.cat([flatten(flop_plays), flop_alone], -1)))
-        processed_turn = selu(self.fc21(t.cat([flatten(turn_plays), turn_alone], -1)))
-        processed_river = selu(self.fc22(t.cat([flatten(river_plays), river_alone], -1)))
+        processed_flop = selu(self.fc20(t.cat([flatten(flop_plays), flop_features], -1)))
+        processed_turn = selu(self.fc21(t.cat([flatten(turn_plays), turn_features], -1)))
+        processed_river = selu(self.fc22(t.cat([flatten(river_plays), river_features], -1)))
         plays = selu(self.fc23(processed_preflop + processed_flop + processed_turn + processed_river))
 
         # add pot, dealer, blinds, dealer, stacks
         pbds = selu(self.fc24(t.cat([pot, stack, opponent_stack, big_blind, dealer], -1)))
 
         # USE ALL INFORMATION (CARDS/ACTIONS/MISC) TO PREDICT THE Q VALUES
-        situation_with_opponent = selu(self.fc25(t.cat([plays, pbds, bh], -1)))
+        situation_with_opponent = selu(self.fc25(t.cat([plays, pbds, board_hand_features], -1)))
         situation_with_opponent = selu(self.fc26(situation_with_opponent))
 
-        return situation_with_opponent, hand_strength, probabilities_of_each_combination_board_hand, probabilities_of_each_combination_board_only
+        return situation_with_opponent
 
 
 class QNetwork(t.nn.Module):
@@ -147,9 +151,7 @@ class QNetwork(t.nn.Module):
                 self.shared_network = shared_network
             else:
                 self.shared_network = SharedNetwork(n_actions, hidden_dim)
-        for i in range(1, 6):
-            setattr(self, 'conv' + str(i), getattr(self.shared_network, 'conv' + str(i)))
-        for i in range(1, 27):
+        for i in range(19, 27):
             setattr(self, 'fc' + str(i), getattr(self.shared_network, 'fc' + str(i)))
         self.fc27 = fc(hdim, hdim)
         self.fc28 = fc(hdim, n_actions)
@@ -161,13 +163,12 @@ class QNetwork(t.nn.Module):
         # https://github.com/harvard-ml-courses/cs281-demos/blob/master/SoftmaxTorch.ipynb
         self.optim = optim.SGD([self.fc27.weight], lr=learning_rate)
 
-    def forward(self, hand, board, pot, stack, opponent_stack, big_blind, dealer, preflop_plays, flop_plays, turn_plays, river_plays):
-        situation_with_opponent, hand_strength, probabilities_of_each_combination_board_hand, probabilities_of_each_combination_board_only = self.shared_network.forward(hand, board, pot, stack, opponent_stack, big_blind, dealer, preflop_plays,
+    def forward(self, cards_features, flop_features, turn_features, river_features, board_hand_features, pot, stack, opponent_stack, big_blind, dealer, preflop_plays, flop_plays, turn_plays, river_plays):
+        situation_with_opponent = self.shared_network.forward(cards_features, flop_features, turn_features, river_features, board_hand_features, pot, stack, opponent_stack, big_blind, dealer, preflop_plays,
                                                                                                                                                                          flop_plays, turn_plays, river_plays)
         q_values = selu(self.fc27(situation_with_opponent))
         q_values = self.fc28(q_values)
-        return q_values, hand_strength, probabilities_of_each_combination_board_hand, probabilities_of_each_combination_board_only
-
+        return q_values
 
     def train(self, states, actions, targets, imp_weights):
         self.optim.zero_grad()
@@ -180,8 +181,7 @@ class QNetwork(t.nn.Module):
         self.optim.step()
         return td_deltas
 
-
-    def compute_loss(x, y, imp_weights):
+    def compute_loss(self, x, y, imp_weights):
         '''
         compute weighted mse loss
         loss for each sample is scaled by imp_weight
@@ -207,17 +207,15 @@ class PiNetwork(t.nn.Module):
                 self.shared_network = shared_network
             else:
                 self.shared_network = SharedNetwork(n_actions, hidden_dim)
-        for i in range(1, 6):
-            setattr(self, 'conv' + str(i), getattr(self.shared_network, 'conv' + str(i)))
-        for i in range(1, 27):
+        for i in range(19, 27):
             setattr(self, 'fc' + str(i), getattr(self.shared_network, 'fc' + str(i)))
         self.fc27 = fc(hdim, hdim)
         self.fc28 = fc(hdim, n_actions)
         # TODO: add softmax loss
 
-    def forward(self, hand, board, pot, stack, opponent_stack, big_blind, dealer, preflop_plays, flop_plays, turn_plays, river_plays):
-        situation_with_opponent, hand_strength, probabilities_of_each_combination_board_hand, probabilities_of_each_combination_board_only = self.shared_network.forward(hand, board, pot, stack, opponent_stack, big_blind, dealer, preflop_plays,
+    def forward(self, cards_features, flop_features, turn_features, river_features, board_hand_features, pot, stack, opponent_stack, big_blind, dealer, preflop_plays, flop_plays, turn_plays, river_plays):
+        situation_with_opponent = self.shared_network.forward(cards_features, flop_features, turn_features, river_features, board_hand_features, pot, stack, opponent_stack, big_blind, dealer, preflop_plays,
                                                                                                                                                                          flop_plays, turn_plays, river_plays)
         pi_values = selu(self.fc27(situation_with_opponent))
         pi_values = softmax(self.fc28(pi_values))
-        return pi_values, hand_strength, probabilities_of_each_combination_board_hand, probabilities_of_each_combination_board_only
+        return pi_values
