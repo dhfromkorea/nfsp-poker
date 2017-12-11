5b46e8cc6dbecc60a44a8f2a387e52ed10f83c8e
dh <dhfromkorea@gmail.com>
2017-12-11 10:07:40 -0500

    fix featurizer training

--- Programs/models/featurizer.py
+++ Programs/models/featurizer.py
@@ -11,13 +11,12 @@ from matplotlib import pyplot as plt
 from sklearn.utils import shuffle
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
-
+import time
 
 from models.q_network import CardFeaturizer1
 from game.utils import variable, moving_avg, initialize_save_folder
 from game.game_utils import Card, cards_to_array
 from game.errors import LoadModelError, NotImplementedError
-
 
 # 60% 20% 20% picked arbitrarily
 HS_DATA_PATH = 'data/hand_eval/'
@@ -28,14 +27,21 @@ HS_DATA_TEST_PATH = HS_DATA_PATH + 'test/'
 PLOT_PATH = 'img/'
 MODEL_PATH = 'saved_models/'
 
+# remove this later
+def moving_avg(arr, window):
+    return [np.mean(arr[i:i+window]) for i in range(len(arr) - window + 1)]
+
 class FeaturizerManager():
     '''
     TODO: support checkpoint saving
     right now only Featurizer1 is supported
     '''
-    def __init__(self, hdim, n_filters, model_name=None, featurizer_type='hs', cuda=False,
-                lr=1e-4, batch_size=128, num_epochs=50, weight_decay=1e-3, plot_freq=10000,
-                 checkpoint_freq=20000, verbose=False):
+    def __init__(self, hdim, n_filters,
+                 model_name=None,
+                 cuda=False, lr=1e-4, batch_size=512,
+                 num_epochs=50, weight_decay=1e-3, plot_freq=10000,
+                 checkpoint_freq=20000,
+                 verbose=False, tensorboard=None):
         self.verbose = verbose
         self.f = CardFeaturizer1(hdim, n_filters, cuda=cuda)
         self.data = {'train': {}, 'val': {}, 'test': {}}
@@ -45,6 +51,7 @@ class FeaturizerManager():
         self.cuda = cuda
         self.save_path = initialize_save_folder(HS_DATA_PATH)
         self.global_step = 0
+        self.tensorboard = tensorboard
 
         self.lr = lr
         self.batch_size = batch_size
@@ -54,7 +61,7 @@ class FeaturizerManager():
         self.checkpoint_freq = checkpoint_freq
 
         if model_name is None:
-            self.model_name = 'c{}_h{}xf{}_model'.format(featurizer_type, hdim, n_filters)
+            self.model_name = 'featurizer_{}'.format(time.time())
         else:
             self.model_name = model_name
         self.model_path = self.save_path + MODEL_PATH + self.model_name
@@ -164,10 +171,6 @@ class FeaturizerManager():
             self.data['test']['y_hs'] = scaler.transform(y_hs_test)
             self.data['test']['y_probs_combi'] = y_probas_combi_test
 
-        #print(x_hand_train, x_board_train, y_hs_train, y_probas_combi_train)
-        #print(x_hand_val, x_board_val, y_hs_val, y_probas_combi_val)
-        #print(x_hand_test, x_board_test, y_hs_test, y_probas_combi_test)
-        #count, bins, _ = plt.hist(y_hs_train, bins=50)
         return train_dataset, val_dataset, test_dataset
 
     def save_model(self, path, epoch_i, batch_i, is_best=False):
@@ -226,47 +229,30 @@ class FeaturizerManager():
         return weight
 
 
-    def train_featurizer1(self, includes_val=True, includes_test=False):
+    def train_featurizer(self):
         '''
         TODO: train_featurizer11 and train_featurizer1 will be merged!
         '''
         train_dataset, val_dataset, test_dataset =\
-        self._preprocess_data(includes_val=includes_val, includes_test=includes_test)
+        self._preprocess_data(includes_val=True, includes_test=True)
         optimizer = t.optim.Adam(self.f.parameters(), lr=self.lr, weight_decay=self.weight_decay)
 
-        if self.verbose:
-            print('loaded train data size')
-            print(len(train_dataset))
-            if includes_val:
-                print('loaded validation data size')
-                print(len(val_dataset))
-            if includes_test:
-                print('loaded test data size')
-                print(len(test_dataset))
-            print('')
-
         for epoch_i in range(self.num_epochs):
-            if epoch_i % self.plot_freq == 0:
-                print('running for {}th epoch'.format(epoch_i+1))
-            # shuffle
             d = self.data['train']
             x_hand_train, x_board_train, y_hs_train, y_probas_combi_train \
           = shuffle(d['x_hand'], d['x_board'], d['y_hs'], d['y_probs_combi'])
 
             # check if weights are NaN
+            start_t = time.time()
             for p in self.f.parameters():
                 if np.isnan(p.data.cpu().numpy()).sum()>0:
                     raise ValueError('nan weights !')
 
-
             for i in range(0, len(train_dataset), self.batch_size):
                 batch_i = i // self.batch_size
-                self.global_step += 1
 
-                # init grad
                 optimizer.zero_grad()
 
-                # sample batch
                 hand = variable(x_hand_train[i:i+self.batch_size], cuda=self.cuda)
                 board = variable(x_board_train[i:i+self.batch_size], cuda=self.cuda)
                 target = variable(y_hs_train[i:i+self.batch_size], cuda=self.cuda).squeeze()
@@ -277,77 +263,77 @@ class FeaturizerManager():
 
                 HS = FeaturizerManager.clip(HS_pred)
                 pred = t.log(HS/(1-HS))
-                # QUESTION: why not average? now SSE not MSE
-                loss = (target - pred)**2  # target already has this format (scaled logit)
-                loss = t.sum(loss)
-                self.train_losses.append(loss.data.cpu().numpy()[0]/self.batch_size)
+                loss = (target - pred).pow(2).mean()
+                raw_loss = float(round(loss.data.cpu().numpy()[0], 2))
+
+                if self.tensorboard is not None:
+                    self.tensorboard.add_scalar_value('train_loss', raw_loss)
 
                 loss.backward()
                 optimizer.step()
-                if self.global_step % self.plot_freq == 0:
-                    #display.clear_output(wait=True)
+            print(time.time()-start_t, 'seconds per epoch')
+            # epoch ended
+            self.validate_featurizer(val_dataset, against_test_data=False)
+            self.save_model(self.model_path, epoch_i, batch_i)
+            self.tensorboard.to_zip('data/hand_eval/tensorboard/{}'.format(time.time()))
+            print('saved model', 'epoch: ', epoch_i, 'batch: ', batch_i)
 
-                    # test loss on a random test sample
-                    # QUESTION: we may be validating for the same samples?
-                    # we could have BatchIter class like cs281 homework
-                    d = self.data['val']
-                    x_hand_val, x_board_val, y_hs_val, y_probas_combi_val \
-                    = shuffle(d['x_hand'], d['x_board'], d['y_hs'], d['y_probs_combi'])
-                    hand = variable(x_hand_val[:1000], cuda=self.cuda)
-                    board = variable(x_board_val[:1000], cuda=self.cuda)
-                    target = variable(y_hs_val[:1000], cuda=self.cuda).squeeze()
-                    self.f.eval()
-
-                    # QUESTION: why add 1e-5?
-                    #HS_pred = self.f.forward(hand, board)[0].squeeze().float() + 1e-5
-                    HS_pred = self.f.forward(hand, board)[0].squeeze().float()
-                    HS = FeaturizerManager.clip(HS_pred)
-                    # QUESTION: why train on validate data? do we even have train member function?
-                    self.f.train()
-                    pred = t.log(HS/(1-HS))
-                    loss = (target - pred)**2
-                    loss = t.sum(loss)
-                    self.val_losses.append(loss.data.cpu().numpy()[0]/1000)
-
-                    train_losses_ = moving_avg(self.train_losses)
-                    if self.verbose:
-                        print('validation loss')
-                        print(np.mean(self.val_losses))
-                        print('train loss')
-                        print(np.mean(train_losses_[-10:]))
-                        print('')
-
-                    # plot
-
-                    fig = plt.figure(figsize=(10,10))
-                    plt.plot(train_losses_, label='train loss (smoothed)')
-                    plt.xlabel('Number of iterations')
-                    plt.ylabel('Loss')
-                    plt.legend(loc='best')
-                    fig.savefig('{}train_loss_e{}b{}'.format(self.plot_path, epoch_i, batch_i), ppi=300, bbox_inches='tight')
-                    plt.close()
-
-                    fig = plt.figure(figsize=(10,10))
-                    plt.plot(self.val_losses, label='validation loss')
-                    plt.xlabel('Number of iterations')
-                    plt.ylabel('Loss')
-                    plt.legend(loc='best')
-                    fig.savefig('{}val_loss_e{}b{}'.format(self.plot_path, epoch_i, batch_i), ppi=300, bbox_inches='tight')
-                    plt.close()
-
-                if self.global_step % self.checkpoint_freq == 0:
-                    # saving checkpoints
-                    self.save_model(self.model_path, epoch_i, batch_i)
-                    print('saved model', 'epoch: ', epoch_i, 'batch: ', batch_i)
         # after training is over, we save the model
+        self.validate_featurizer(test_dataset, against_test_data=True)
         self.save_model(self.model_path, epoch_i, batch_i, is_best=True)
+        self.tensorboard.to_zip('data/hand_eval/tensorboard/{}'.format(time.time()))
         print('saved final model', 'epoch: ', epoch_i, 'batch: ', batch_i)
 
+
+    def validate_featurizer(self, dataset, against_test_data=False):
+        if against_test_data:
+            d = self.data['test']
+        else:
+            d = self.data['val']
+
+        # hardcode epochs
+        x_hand, x_board, y_hs, y_probas_combi = shuffle(d['x_hand'], d['x_board'], d['y_hs'], d['y_probs_combi'])
+
+        for i in range(0, len(dataset), self.batch_size):
+            batch_i = i // self.batch_size
+            hand = variable(x_hand[i:i+self.batch_size], cuda=self.cuda)
+            board = variable(x_board[i:i+self.batch_size], cuda=self.cuda)
+            target = variable(y_hs[i:i+self.batch_size], cuda=self.cuda).squeeze()
+
+            self.f.eval()
+            if len(hand) != self.batch_size:
+                break
+
+            HS_pred = self.f.forward(hand, board)[0].squeeze().float()
+            HS = FeaturizerManager.clip(HS_pred)
+
+            self.f.train()
+            # for numerical stable
+            m = variable(np.array([1e-6]), cuda=self.cuda)
+            pred = t.log(t.max(HS/(1-HS), m))
+            loss = (target - pred).pow(2).mean()
+            loss = float(round(loss.data.cpu().numpy()[0], 3))
+
+            if self.tensorboard is not None:
+                if against_test_data:
+                    self.tensorboard.add_scalar_value('test_loss', loss)
+                else:
+                    self.tensorboard.add_scalar_value('validation_loss', loss)
+
+
     def plot(self):
-        pass
+        fig = plt.figure(figsize=(10,10))
+        plt.plot(train_losses_, label='train loss (smoothed)')
+        plt.xlabel('Number of iterations')
+        plt.ylabel('Loss')
+        plt.legend(loc='best')
+        fig.savefig('{}train_loss_e{}b{}'.format(self.plot_path, epoch_i, batch_i), ppi=300, bbox_inches='tight')
+        plt.close()
 
-    def test(self):
-        pass
-
-    def log_results(self):
-        pass
+        fig = plt.figure(figsize=(10,10))
+        plt.plot(self.val_losses, label='validation loss')
+        plt.xlabel('Number of iterations')
+        plt.ylabel('Loss')
+        plt.legend(loc='best')
+        fig.savefig('{}val_loss_e{}b{}'.format(self.plot_path, epoch_i, batch_i), ppi=300, bbox_inches='tight')
+        plt.close()
--- Programs/train_featurizer.py
+++ Programs/train_featurizer.py
@@ -1,16 +1,40 @@
 from models.featurizer import FeaturizerManager
 
+from pycrayon import CrayonClient
+import time
+
 FEATURIZER_NAME = 'chs_h50xf10_model_e9b22079_best'
 SAVED_FEATURIZER_PATH = 'data/hand_eval/2017_11_30/saved_models/' + FEATURIZER_NAME
+
+
+def setup_tensorboard(exp_id, cur_t, hostname, port):
+    exp_filename = '{}_{}'.format(cur_t, exp_id)
+    tb = CrayonClient(hostname=hostname, port=port)
+    try:
+        tb_experiment = tb.create_experiment(exp_filename)
+    except:
+        # flush the data anew
+        tb.remove_experiment(exp_filename)
+        tb_experiment = tb.create_experiment(exp_filename)
+    return tb_experiment, tb
 
 if __name__ == '__main__':
     # TODO: arg parser
     cuda = True
     verbose = False
-    #fm = FeaturizerManager(hdim=50, n_filters=10,
-    #                       plot_freq=10000, batch_size=64,
-    #                       checkpoint_freq=20000,
-    #                       cuda=cuda, verbose=verbose)
-    #fm.train_featurizer1()
-    f = FeaturizerManager.load_model(SAVED_FEATURIZER_PATH, cuda=cuda)
+    exp_id = 'featurizer1_train'
+    plot_freq = 100
+    checkpoint_freq = 1000
+    tb_experiment, _ = setup_tensorboard(exp_id, time.time(), 'localhost', '8889')
+
+    fm = FeaturizerManager(hdim=50,
+                           n_filters=10,
+                           plot_freq=plot_freq,
+                           batch_size=1024,
+                           num_epochs=100,
+                           checkpoint_freq=checkpoint_freq,
+                           tensorboard=tb_experiment,
+                           cuda=cuda,
+                           verbose=verbose)
+    fm.train_featurizer()
 
